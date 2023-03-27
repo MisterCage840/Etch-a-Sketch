1b6be7b9cbcd118519659a47d838adb50ec97cef
MisterCage <mohammad.barbarji@icloud.com>
2023-03-28 03:35:10 +0400

    .

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
+const mainContainer = document.querySelector(".mainContainer");
+const stat= document.querySelector(".status");
+
+stat.textContent = "Brush on";
+
 for(let i=1; i<=16;i++){
     let newDiv = document.createElement('div');
     newDiv.classList = "container";
-    document.body.appendChild(newDiv);
+    mainContainer.appendChild(newDiv);
     for(let f=1;f<=16;f++){
         let insideDiv = document.createElement('div');
         insideDiv.classList = "contEl";
         newDiv.appendChild(insideDiv);
     }
 }
+document.body.addEventListener("keydown",(event)=>{
+    if(event.isComposing || event.key === 'a'){
+        console.log(event.key);
+        if(stat.textContent == "Brush on"){
+            stat.textContent = "Brush off";
+            stat.style.backgroundColor = "grey";
+        } 
+        else if(stat.textContent == "Brush off"){
+            stat.textContent = "Brush on";
+            stat.style.backgroundColor = "lightgreen";
+        }
+    }
+})
+
+const grids = document.querySelectorAll(".contEl");
+grids.forEach(grid=>{
+    grid.addEventListener("mousemove", ()=>{
+        if(stat.textContent == "Brush on")
+            grid.style.backgroundColor = "black";
+        })
+    });
