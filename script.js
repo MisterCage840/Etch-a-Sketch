8c1a20472ae36566c22ca7e82153e14c46b7e417
MisterCage <mohammad.barbarji@icloud.com>
2023-04-22 23:55:45 +0400

    .

--- script.js
+++ script.js
@@ -1,36 +1,14 @@
-const mainContainer = document.querySelector(".mainContainer");
-const stat= document.querySelector(".status");
+const mainGrid = document.querySelector('.mainGrid');
+const mainContainer = document.querySelector('.mainContainer');
 
-stat.textContent = "Brush on";
-
-for(let i=1; i<=16;i++){
-    let newDiv = document.createElement('div');
-    newDiv.classList = "container";
-    mainContainer.appendChild(newDiv);
-    for(let f=1;f<=16;f++){
-        let insideDiv = document.createElement('div');
-        insideDiv.classList = "contEl";
-        newDiv.appendChild(insideDiv);
+for(let i=1; i<=16; i++){
+    let blockGrid = document.createElement('div');
+    blockGrid.classList = 'blockGrid';
+    for(let f=1; f<=16;f++){
+        let smallGrid = document.createElement('div');
+        smallGrid.classList = 'smallGrid';
+        blockGrid.appendChild(smallGrid);
     }
+    mainGrid.appendChild(blockGrid);
 }
-document.body.addEventListener("keydown",(event)=>{
-    if(event.isComposing || event.key === 'a'){
-        console.log(event.key);
-        if(stat.textContent == "Brush on"){
-            stat.textContent = "Brush off";
-            stat.style.backgroundColor = "grey";
-        } 
-        else if(stat.textContent == "Brush off"){
-            stat.textContent = "Brush on";
-            stat.style.backgroundColor = "lightgreen";
-        }
-    }
-})
 
-const grids = document.querySelectorAll(".contEl");
-grids.forEach(grid=>{
-    grid.addEventListener("mousemove", ()=>{
-        if(stat.textContent == "Brush on")
-            grid.style.backgroundColor = "black";
-        })
-    });
